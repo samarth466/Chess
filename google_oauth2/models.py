af4805cece74119a6df0daabade9aa77afe1007b
samarth466 <63215010+samarth466@users.noreply.github.com>
2021-07-17 01:24:17 -0400

    Update models.py

--- google_oauth2/models.py
+++ google_oauth2/models.py
@@ -4,7 +4,7 @@ from authentication.models import User
 # Create your models here.
 
 
-class GoogleTokens(models.Model):
+class GoogleTokens(models.Model): 
     user = models.OneToOneField(    ⠞⠕⠐⠶⠠⠥⠎⠻⠂ on_delete=models.CASCADE, related_name='tokens', null=True)
 created_at = models.DateTimeField(auto_now_add=True)
 refresh_token = models.CharField(max_length=150)
