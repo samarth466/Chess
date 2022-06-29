18d48c5a8ad073711e5ec59047cd1e08afc5e010
Samarth Saxena <samarths466@gmail.com>
2022-06-29 13:13:28 -0400

    added a new chess.GamingScripts.pieces.piece.Piece subclass called Empty() to handle squares without pieces on them.

---
+++ Chess/GamingScripts/pieces/empty.py
@@ -0,0 +1,24 @@
+import pygame
+from .piece import Piece
+from utils.types import Squares
+
+
+class Empty(Piece):
+
+    def __init__(self, image: str, file: str, rank: int, color: pygame.Color, min_x: int, max_x: int, min_y: int, max_y: int, square_width: int, square_height: int, win_width: int, win_height: int) -> None:
+        pygame.init()
+        super().__init__(image, file, rank, 'none', color)
+        self.min_x = min_x
+        self.max_x = max_x
+        self.min_y = min_y
+        self.max_y = max_y
+        self.square_width = square_width
+        self.square_height = square_height
+        self.win_width = win_width
+        self.win_height = win_height
+        self.x, self.y = self.piece_x, self.piece_y = get_window_pos(
+            self.file, self.rank, self.possible_files)
+        self.attacked_pieces = []
+
+    def move(self, win: pygame.Surface, squares: Squares):
+        raise NotImplementedError()
